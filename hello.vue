d41f1ee803d6e51616c166bc31c904a7c72609ab
suchanglin <suchanglin2015@gmail.com>
2022-03-20 17:52:13 +0800

    want to delete branch

--- hello.vue
+++ hello.vue
@@ -2,6 +2,7 @@
     <div>
         <p>this is a file for me test github and git</p>
         <p>file edit selection view go run terminal help</p>
+        <p>i want to delete dev branch</p>
     </div>
 </template>
 
